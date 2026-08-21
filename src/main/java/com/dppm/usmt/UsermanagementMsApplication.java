54ca06829fb18dfe768aaecb2065949f50ebfaef
Jashobanta <jashobantapatra35@gmail.com>
2026-08-21 21:18:39 +0530

    project framework setup

---
+++ src/main/java/com/dppm/usmt/UsermanagementMsApplication.java
@@ -0,0 +1,13 @@
+package com.dppm.usmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dppm/usmt/UsermanagementMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dppm.usmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
